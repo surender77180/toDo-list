48706ad772f13d161741948b01e2557bc3159032
surender77180 <surender791999@gmail.com>
2023-07-30 09:38:24 +0530

    multer-add

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const session = require("express-session");
 const path = require("path");
 const fs = require("fs");
 const app = express();
+const multer = require("multer");
+const { render } = require("ejs");
+const upload = multer({ dest: "uploads/" })
+
+
 const port = 4567;
-
 app.set("view engine", "ejs");
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// single can use single file upload
+// array can use multiple file upload
+app.use(upload.single("pic")); //middle ware
 app.use(express.json()); //middle ware//
 app.use(express.urlencoded({ extended: true }));
+
 
 app.use(session({
     secret: "i love this my village..",
@@ -18,16 +26,18 @@ app.use(session({
     // cookie: {secure: true}
 }))
 
-function serverFile(url, filename) {
+
+function serverFile(url, filename) {  //
     app.get(url, (req, res) => {
         res.sendFile(__dirname + "/" + filename);
     })
 }
 
+
 serverFile("/style.css", "style.css");
 serverFile("/logreg.css", "logreg.css")
 serverFile("/index.js", "index.js");
-serverFile("/reg", "/reg.html");
+
 
 class Item {
     static list = {};
@@ -55,7 +65,7 @@ class Item {
         }
     }
 
-    static delItem(email, id){
+    static delItem(email, id) {
         try {
             const data = fs.readFileSync("data.json");
             const useritems = JSON.parse(data);
@@ -82,13 +92,12 @@ class Item {
             const data = fs.readFileSync("data.json");
             const useritems = JSON.parse(data);
 
-            if(!(email in useritems)){
+            if (!(email in useritems)) {
                 useritems[email] = "[]";
             }
-            console.log(useritems, email);
-
+            // console.log(useritems, email);
             return JSON.parse(useritems[email]);
-            
+
         } catch (err) {
             console.log(err);
             return null;
@@ -99,6 +108,7 @@ class Item {
         this.task = task;
         this.isDone = false;
         this.id = Item.genRandomId();
+        this.imgPath = "";
     }
 };
 
@@ -134,6 +144,12 @@ function saveUser(user) {
     }
 }
 
+
+app.get("/reg", (req, res) => {
+    res.render("reg", {});
+})
+
+
 app.get("/", (req, res) => {
 
     if (req.session.isLoggedIn) {
@@ -146,57 +162,44 @@ app.get("/", (req, res) => {
             res.send("User not Found...");
         } else {
             const items = Item.getItems(email);
-            const showName= user.username.charAt(0).toUpperCase() + user.username.slice(1)
+            const showName = user.username.charAt(0).toUpperCase() + user.username.slice(1)
             res.render("index", { logingInUser: showName, items });
         }
-        // fs.readFile("./password.json", (err, data) => {
-        //   if (err) {
-        //     res.send("file not readable...");
-        //   } else {
-        //     const users = JSON.parse(data);
-        //     const user = users[email];
-        //     if (!user) {
-        //       res.send("User Not Found...");
-        //     } else {
-        //       res.render("index", { logingInUser: user.username, items: Item.list });
-        //     }
-        //   }
-        // })
     } else {
         res.redirect("login");
     }
 });
 
-app.get("/login", (req, res) => {
 
-    if (req.session.isLoggedIn) {
-        res.redirect("/");
-    } else {
-        res.sendFile(path.join(__dirname, "login.html"));
-    }
+app.get("/login", (req, res) => {
+    res.render("login", { error: "" });
 });
+
 
 app.post("/item", (req, res) => {
     const task = req.body.task;
+    console.log(req.file);
     const email = req.session.email;
     const item = new Item(task);
+    item.imgPath = req.file.path;
     Item.addItem(email, item);
     res.redirect("/");
 });
+
 
 app.get("/checkitem/:id/:isDone", (req, res) => {
     const { params: { id, isDone } } = req;
     const email = req.session.email;
     const item = Item.findItem(email, id);
-    console.log(item);
     if (!item) {
         res.sendStatus(404);
     } else {
-        item.isDone =! JSON.parse(isDone);
+        item.isDone = !JSON.parse(isDone);
         Item.addItem(email, item);
         res.redirect("/");
     }
 });
+
 
 app.get("/delitem/:id", (req, res) => {
     const { params: { id } } = req;
@@ -209,8 +212,6 @@ app.get("/delitem/:id", (req, res) => {
         res.redirect("/");
     }
 });
-
-app.listen(port);
 
 // authorise session//
 app.post("/login", function (req, res) {
@@ -227,27 +228,6 @@ app.post("/login", function (req, res) {
         req.session.email = email;
         res.redirect("/")
     }
-
-    // fs.readFile("./password.json", (err, data) => {
-    //   if (err) {
-    //     res.send("file not readable...");
-    //   } else {
-    //     const users = JSON.parse(data);
-    //     const user = users[email];
-    //     if (!user) {
-    //       // res.send("User Not Found...");
-    //       res.render("login", { error: "Invalid username..." });
-
-    //     } else if (user.password != password) {
-    //       // res.send("wrong pass...");
-    //       res.render("login", { error: "Invalid password..." });
-    //     } else {
-    //       req.session.isLoggedIn = true;
-    //       req.session.email = email;
-    //       res.redirect("/")
-    //     }
-    //   }
-    // })
 });
 
 //register new user//
@@ -261,54 +241,13 @@ app.post("/adduser", (req, res) => {
     };
     const success = saveUser(user);
     if (!success) {
-        res.render("login", {error: "Email already registerd"});
+        res.render("login", { error: "Email already registerd" });
     } else {
-        res.render("login", {error: "Registered succesfully..."});
+        res.render("login", { error: "Registered succesfully..." });
     }
-
-    // fs.readFile("./password.json", (err, data) => {
-    //   if (err) {
-    //     res.send("file not readable...");
-    //   } else {
-    //     const users = JSON.parse(data);
-    //     if (users[email]) {
-    //       res.send("user allready exits...");
-    //     } else {
-    //       const user = {
-    //         username, password, email
-    //       };
-    //       users[email] = user;
-    //       fs.writeFile("./password.json", JSON.stringify(users), err => {
-    //         if (err) {
-    //           res.send("unable to user...");
-    //         } else {
-    //           res.send("Registered succesfully...")
-    //         }
-    //       })
-    //     }
-    //   }
-    // })
 })
 
 //add username & logout btn//
-
-// app.get("/getuser", (req, res) => {
-//     const email = req.session.email;
-//     fs.readFile("./password.json", (err, data) => {
-//         if (err) {
-//             res.send("file not readable...");
-//         } else {
-//             const users = JSON.parse(data);
-//             const user = users[email];
-//             if (!user) {
-//                 res.send("User Not Found...");
-//             } else {
-//                 res.send(user.username);
-//             }
-//         }
-//     })
-// })
-
 app.get("/logout", (req, res) => {
     req.session.isLoggedIn = false;
     delete req.session.email;
@@ -318,3 +257,7 @@ app.get("/logout", (req, res) => {
 app.get("/showname/:name", (req, res) => {
     res.send(req.params.name);
 })
+
+
+
+app.listen(port);
